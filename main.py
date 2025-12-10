547ac431fda82c0d5882e3c62e8c3d944da5380c
NLdouglas <douglasleodoro.qi@gmail.com>
2025-12-10 09:30:40 -0300

    add all

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-# SendEmails
-Envio de emails em massa.
+import pandas as pd 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
 
-
+sender = ''
+appPassword = ''
+newHire = pd.read_excel('./NewHireEmail.xlsx')
+print(newHire)
+#newHire = newHire.fillna("")
+#newHire = newHire.astype(str)
 
 for user, newHires in newHire.iterrows():
     msg = MIMEMultipart()
     msg['Subject'] = 'Login SAP'
-    message = f"Hello {newHires['email']}, this is your SAP UserID and E-mail \n{newHires['infos']}"
+    message = f"Hi {newHires['nome']}, \nthis is your SAP UserID and E-mail: \n {newHires['infos']}."
     msg['From'] = sender
     msg['To'] = newHires['email']
     msg.attach(MIMEText(message, 'plain'))
---
+++ test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+newHire = pd.read_excel('./NewHireEmail.xlsx')
+print(newHire)
